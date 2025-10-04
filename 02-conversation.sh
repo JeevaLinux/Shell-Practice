0dfe87c5850d8729887c1a33b8bc33bfff2613cf
Jeeva <endlurijeeva93@mail.com>
2025-10-04 19:59:12 +0530

    variables

--- 02-conversation.sh
+++ 02-conversation.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo "Rmesh: : Hi SUresh, How are you?"
+echo "Rmesh: : Hi Suresh, How are you?"
 echo "Suresh: : Hello Ramesh, I am fine.How are you?"
 echo "Ramesh: : Waht are you learning recently?"
 echo "Suresh: : I am learning shell scripting Ramesh"
---
+++ 03-variables.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+PERSON1=$1
+PERSON2=$2
+
+
+echo "$PERSON1: : Hi $PERSON2, How are you?"
+echo "$PERSON2: : Hello $PERSON1, I am fine.How are you?"
+echo "$PERSON1: : Waht are you learning recently?"
+echo "$PERSON2: : I am learning shell scripting $PERSON1"
