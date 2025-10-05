960447f1cc43aba0b74c53df42c9736cc7ca1dc7
Jeeva <endlurijeeva93@mail.com>
2025-10-05 19:35:20 +0530

    session-06

--- 07-conditions.sh
+++ 07-conditions.sh
@@ -3,8 +3,8 @@
 echo "please enter the number"
 read NUMBER
 
-if [ $(($NUMBER % 2)) -eq 0]; then
+if [ $(($NUMBER % 2)) -eq 0 ]; then
     echo "Given number $NUMBER is EVEN"
 else
-    echo "GIven number $NUMBER is ODD"
+    echo "Given number $NUMBER is ODD"
 fi
