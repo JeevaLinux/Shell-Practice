cf8e55a3cf5144e33757bebc30daa7e98d54e0d3
Jeeva <endlurijeeva93@mail.com>
2025-10-05 19:33:31 +0530

    session-06

--- 05-conditions.sh
+++ 05-conditions.sh
@@ -6,9 +6,9 @@ if [ $NUMBER -lt 10 ]; then
     echo "Given Number $NUMBER is less than 10"
 elif [ $NUMBER -eq 10 ]; then
     echo "Given Number $NUMBER is equal to 20"
-        
+
 else
-    echo "Given number $NUMBER is greater than or equal to 10"
+    echo "Given number $NUMBER is greater than 10"
 fi
 
 # -gt ==>greater than
