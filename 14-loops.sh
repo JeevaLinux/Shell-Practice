14599da5a3f352576360b165224276bb858a2d18
Jeeva <endlurijeeva93@mail.com>
2025-10-06 12:57:10 +0530

    session-06

--- 14-loops.sh
+++ 14-loops.sh
@@ -31,5 +31,16 @@ VALIDATE(){ #functions receive inputs throigh args, just like shell script
 
 # $@
 for package in $@
-do  echo "Package is: $package"
+do  
+    #check packageis already installed or not
+    dnf list installed $package &>>$LOG_FILE
+
+    #if exit status is 0, already installed. -ne 0 need to install it
+
+    if [ $? -ne 0 ]; then
+        dnf install $package -y &>>$LOG_FILE
+        VALIDATE $? "$package"
+    else
+        echo -e "$package already installed ...$Y SKIPPING $N"
+    fi
 done
