ef8f520bf9b6eaf51b2aad635e333d57f12cf4ca
Jeeva <endlurijeeva93@mail.com>
2025-10-06 12:49:10 +0530

    session-06

--- 12-logs.sh
+++ 12-logs.sh
@@ -12,6 +12,7 @@ LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
 
 mkdir -p $LOGS_FOLDER
 echo "script started executed at: $(date)" | tee -a $LOG_FILE
+
 if [ $USERID -ne 0 ]; then
     echo "ERROR:: Please run this script with root privelege"
     exit 1 #failure is other than 0
