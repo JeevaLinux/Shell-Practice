46371b8973f19c64f02903a293bb546b930e9b3b
Jeeva <endlurijeeva93@mail.com>
2025-10-16 11:53:03 +0530

    session-06

---
+++ 21-backup.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+USERID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+LOGS_FOLDER="/var/log/shell-script"
+SCRIPT_NAME=$( echo $0 | cut -d "." -f1 )
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
+
+mkdir -p $LOGS_FOLDER
+echo "script started executed at: $(date)" | tee -a
+
+if [ $USERID -ne 0 ]; then
+    echo "ERROR:: Please run this script with root privelege"
+    exit 1 #failure is other than 0
+fi
+
+USAGE() {
+    echo -e "$R USAGE: : sudo sh 21-backup.sh <SOURCE_DIR> <DEST_DIR> <DAYS> [Optional, default 14 days]"
+}
+if [ $# -lt 2 ]; then
+    USAGE
+fi
