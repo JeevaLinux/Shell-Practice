822bb361aba331034c0f941a3723b80daaef92d5
Jeeva <endlurijeeva93@mail.com>
2025-10-16 12:02:35 +0530

    session-06

--- 21-backup.sh
+++ 21-backup.sh
@@ -40,6 +40,5 @@ FILES=$(find $SOURCE_DIR -name "*.log" -type -f -mtime +14)
 if [ ! -z "${FILES}" ]; then
     echo "Files found"
 else
-    echo "No files to archeive ... $Y SKIPPING $N"
+    echo -e "No files to archeive ... $Y SKIPPING $N"
 fi
-        
