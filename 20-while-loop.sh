45897e5a5f1b5c47deb1d22cac10e8b9a882cc95
Jeeva <endlurijeeva93@mail.com>
2025-10-16 11:37:08 +0530

    session-06

--- 20-while-loop.sh
+++ 20-while-loop.sh
@@ -2,14 +2,25 @@
 
 #to read the files we will use forloop
 
-count=5
-echo "Starting Countdown..."
+# count=5
+# echo "Starting Countdown..."
 
-while [ $count -gt 0 ]
-do
-	echo "Time left: $count"
-    sleep 1 #Pause for 1 second
-    count=$((count - 1)) #DEcrement the count
-done    
+# while [ $count -gt 0 ]
+# do
+# 	echo "Time left: $count"
+#     sleep 1 #Pause for 1 second
+#     count=$((count - 1)) #DEcrement the count
+# done    
 
-echo "Times up!"
+# echo "Times up!"
+
+while IFS= read -r line; do
+    #Process each line here
+    echo "Processing line: $line"
+    #Ex: if you can peform other operations with the $line variable
+    #for instance:
+    #some_command "$line"
+    #if [[ "$line" == "specific_text" ]]; then
+    #   echo "Found specific text"
+    #fi
+done < 17-script-1.sh    
