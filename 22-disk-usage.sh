f9484bbb92719638d91956a91de55ce567dd28a4
Jeeva <endlurijeeva93@mail.com>
2025-10-16 16:20:39 +0530

    session-06

--- 22-disk-usage.sh
+++ 22-disk-usage.sh
@@ -2,13 +2,15 @@
 
 DISK_USAGE=$(df -hT | grep -v Filesystem)
 DISK_THRESHOLD=2 #in projects we will keep it as 75%
-
+MESSAGE=""
 while IFS= read -r line
 
 do
     USAGE=$(echo $line | awk '{print $6}' | cut -d "%" -f1)
     PARTITION=$(echo $line | awk '{print $7}')
     if [ $USAGE -ge $DISK_THRESHOLD ]; then
-        echo "High Usage on $PARTITION: $USAGE"
+        MESSAGE="High Disk Usage on $PARTITION: $USAGE %"
     fi    
-done <<< $DISK_USAGE    
+done <<< $DISK_USAGE
+
+echo "Message Body: $MESSAGE"
